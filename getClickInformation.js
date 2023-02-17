3eb2a592259ecc2cf89df421ae19cc96ee81308a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-17 19:00:16 +0530

    Modify cardId:number, operation:string

--- getClickInformation.js
+++ getClickInformation.js
@@ -1,12 +1,12 @@
 const getItemOperation = function (id) {
   const idSubstringArray = id.split("-", 2);
   const operation = idSubstringArray[1];
-  return operation;
+  return operation.toString();
 };
 
 const getCardId = function (id) {
   const cardId = id.split("-", 1).join();
-  return cardId;
+  return parseInt(cardId);
 };
 
 const getClickInformation = function checkWhereTheClickComesFrom(event) {
@@ -14,9 +14,6 @@ const getClickInformation = function checkWhereTheClickComesFrom(event) {
   const cardId = getCardId(eventTargetId);
   const itemOperation = getItemOperation(eventTargetId);
   return { cardId, itemOperation };
-
-  event.target.id === "plus" ? "addItem" : event.target.id === "minus";
-  console.log(event.target);
 };
 
 export default getClickInformation;
