35c6ed3716c94f063b7adc44874c80443930ec7f
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-17 17:36:37 +0530

    Add new data generated with the help of GPT

--- data.js
+++ data.js
@@ -1,23 +1,48 @@
-const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2
-    }
-]
+let foodItemsArray = [];
+export default foodItemsArray = [
+  {
+    name: "The Bug Squasher Calzone",
+    ingredients: ["pepperoni", "sausage", "mushrooms", "green peppers"],
+    id: 0,
+    price: 14,
+    imgName: "calzone.jpg",
+    description:
+      "Squash those pesky bugs with our hearty calzone, loaded with pepperoni, sausage, mushrooms, and green peppers. Perfect for fueling late-night coding sessions!",
+  },
+  {
+    name: "The Binary Burger",
+    ingredients: ["beef patty", "cheddar cheese", "bacon", "lettuce"],
+    id: 1,
+    price: 10,
+    imgName: "burger.webp",
+    description:
+      "Satisfy your hunger with our Binary Burger, topped with cheddar cheese, bacon, and lettuce. It's a 1 or a 0 decision, and the answer is definitely YES!",
+  },
+  {
+    name: "The Programming Penne",
+    ingredients: ["penne", "tomato sauce", "meatballs", "parmesan cheese"],
+    id: 2,
+    price: 12,
+    imgName: "pasta.jpg",
+    description:
+      "Our Programming Penne is the perfect way to code and carb load! Enjoy delicious penne pasta with juicy meatballs and parmesan cheese, and let the creative juices flow.",
+  },
+  {
+    name: "The Error-Free Salad",
+    ingredients: ["spinach", "strawberries", "almonds", "feta cheese"],
+    id: 3,
+    price: 8,
+    imgName: "salad.jpg",
+    description:
+      "We guarantee you'll never encounter an error with our Error-Free Salad. Packed with healthy spinach, sweet strawberries, crunchy almonds, and tangy feta cheese, it's a delicious way to stay on top of your game!",
+  },
+  {
+    name: "The Code Refactor Wrap",
+    ingredients: ["grilled chicken", "avocado", "spinach", "ranch dressing"],
+    id: 4,
+    price: 11,
+    imgName: "wrap.jpg",
+    description:
+      "Refactor your lunch game with our Code Refactor Wrap, stuffed with juicy grilled chicken, creamy avocado, fresh spinach, and tangy ranch dressing. It's a refactor you can definitely get behind!",
+  },
+];
